7855102b59f64686908f1face8190eca5b3d1004
archisuko <99643416+archisuko@users.noreply.github.com>
2022-02-17 11:26:07 +0300

    Add files via upload

---
+++ name/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Name
+{
+    class Name
+    {
+        public static void Main(string[] args)
+        {
+                Console.WriteLine("helooo");
+                Console.WriteLine("İsim:");
+                string name = Console.ReadLine();
+                Console.WriteLine("Soyisim:");
+                string surName = Console.ReadLine();
+                Console.WriteLine("Yaş:");
+                int age = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine(name + " " + surName + " " + age + " yaşında");
+        }
+    }
+}
