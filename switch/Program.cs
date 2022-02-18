8eabaac454cfe88aaef8a45e0387a575ebaec490
archisuko <99643416+archisuko@users.noreply.github.com>
2022-02-18 09:21:02 +0300

    Add files via upload

---
+++ switch/Program.cs
@@ -0,0 +1,29 @@
+﻿int month = DateTime.Now.Month;
+
+Console.WriteLine(month + ". aydasın");
+
+switch (month)
+{
+    case 12:
+    case 1:
+    case 2:
+        Console.WriteLine("Kış Mevsimi");
+        break;
+    case 3:
+    case 4:
+    case 5:
+        Console.WriteLine("İlkbahar Mevsimi");
+        break;
+    case 6:
+    case 7:
+    case 8:
+        Console.WriteLine("Yaz Mevsimi");
+        break;
+    case 9:
+    case 10:
+    case 11:
+        Console.WriteLine("Sonbahar Mevsimi");
+        break;
+    default:
+    break;
+}
