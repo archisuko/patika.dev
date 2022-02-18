5b41f2e48e567fa38c23de361e45674f3433c247
archisuko <99643416+archisuko@users.noreply.github.com>
2022-02-18 08:57:35 +0300

    Add files via upload

---
+++ if-else/Program.cs
@@ -0,0 +1,17 @@
+﻿int month = DateTime.Now.Month;
+
+Console.WriteLine(month + ". aydasın");
+
+if (month<5) {
+    Console.WriteLine("Yaza az kaldı...");
+} else if (month<9) {
+    Console.WriteLine("Yazdasın :)");
+} else {
+    Console.WriteLine("Sonraki yılı bekle :(");
+}
+
+string message = month<5 ? "Yılın başı" : "Yılın sonu";
+Console.WriteLine(message);
+
+message = 5<month && month<9 ? "Yıl ortası" : month<5 ? "Yeni yıldasın" : "Yılın sonu";
+Console.WriteLine(message);
