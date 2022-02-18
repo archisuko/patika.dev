cc663ca4c649b0fe556e94ea4f68772399e2c86d
archisuko <99643416+archisuko@users.noreply.github.com>
2022-02-18 08:24:59 +0300

    Add files via upload

---
+++ try-catch-finally/Program.cs
@@ -0,0 +1,25 @@
+﻿try 
+{
+    Console.WriteLine("Doğum yılınız:");
+    int year = Convert.ToInt32(Console.ReadLine());
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Geçerli bir yıl giriniz!");
+    Console.WriteLine(ex.Message.ToString());
+}
+finally 
+{
+    Console.WriteLine("Tamamdır...");
+}
+
+try
+{
+    int x = int.Parse(null);
+}
+catch (ArgumentNullException ex)
+{
+    Console.WriteLine("Boş değer...");
+    Console.WriteLine(ex);
+}
