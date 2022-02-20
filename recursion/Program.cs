8a8e48ff30c7b398fcc87f9ddc17bf45f5426f58
archisuko <99643416+archisuko@users.noreply.github.com>
2022-02-20 12:46:27 +0300

    Add files via upload

---
+++ recursion/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int Factor (int i) {
+    if (i==1) {
+        return 1;
+    }
+    return i*Factor(i-1);
+}
+
+Console.WriteLine(Factor(5));
+
+int Fibonacci (int i) {
+    if (i==2) {
+        return 1;
+    } else if (i==1){
+        return 0;
+    } else {
+        return Fibonacci(i-1)+Fibonacci(i-2);
+    }
+}
+
+Console.WriteLine(Fibonacci(5));
+Console.WriteLine(Fibonacci(10));
+Console.WriteLine(Fibonacci(11));
+Console.WriteLine(Fibonacci(12));
