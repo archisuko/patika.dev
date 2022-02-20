837d040c5504d15934bd3d9f93343e0f6b291eca
archisuko <99643416+archisuko@users.noreply.github.com>
2022-02-20 12:16:57 +0300

    Add files via upload

---
+++ methods/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+int x = 5;
+int y = 7;
+Console.WriteLine(x*y);
+
+int Carpma(int i, int j) {
+    return i*j;
+}
+Console.WriteLine(Carpma(x, y));
+Console.WriteLine(x);
+Console.WriteLine(y);
+
+int artıCarpma(ref int i,ref int j) {
+    i++;
+    j++;
+    return i*j;
+}
+Console.WriteLine(artıCarpma(ref x,ref y));
+Console.WriteLine(x);
+Console.WriteLine(y);
