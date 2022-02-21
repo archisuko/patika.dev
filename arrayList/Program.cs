3c7fa47dc8dc7a8a432b4916e1000f9e0a4cc152
archisuko <99643416+archisuko@users.noreply.github.com>
2022-02-21 20:46:00 +0300

    Add files via upload

---
+++ arrayList/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+
+ArrayList list = new ArrayList();
+list.Add("Şükrü");
+list.Add(24);
+list.Add('F');
+list.Add(true);
+
+Console.WriteLine(list[0]);
+
+foreach (var i in list) {
+    Console.WriteLine(i);
+}
+
+int[] numbers = {23,454,3,56,5656};
+list.Add(numbers);
+
+
+list.Clear();
