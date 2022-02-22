fc369f98da9ad8ee15df74c731f8b2448ace3697
archisuko <99643416+archisuko@users.noreply.github.com>
2022-02-22 10:59:38 +0300

    Add files via upload

---
+++ dictionary/Program.cs
@@ -0,0 +1,21 @@
+﻿
+Dictionary<int,string> school = new();
+
+school.Add(25,"Ali");
+school.Add(23,"Mehmet");
+school.Add(18,"Şükrü");
+
+foreach (var i in school) {
+    Console.WriteLine(i);
+}
+
+Console.WriteLine(school.Count);
+Console.WriteLine(school.ContainsKey(25));
+Console.WriteLine(school.ContainsValue("Mehmet"));
+
+foreach (var i in school.Values) {
+    Console.WriteLine(i);
+}
+foreach (var i in school.Keys) {
+    Console.WriteLine(i);
+}
