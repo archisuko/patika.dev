9d04e8f4753ebdec9a38792cc213b3fd2e8be3a4
archisuko <99643416+archisuko@users.noreply.github.com>
2022-02-23 13:47:52 +0300

    Add files via upload

---
+++ enum/Program.cs
@@ -0,0 +1,11 @@
+﻿
+if (DateTime.Now.Month <= (int)months.Şubat) {
+    Console.WriteLine("Kış ayındasınız...");
+}
+enum months {
+    Ocak = 1, Şubat, 
+    Mart, Nisan, Mayıs,
+    Haziran, Temmuz, Ağustos,
+    Eylül, Ekim, Kasım,
+    Aralık
+}
