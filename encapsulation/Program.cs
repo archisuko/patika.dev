3a14ed4924b54e4a5e7501312bcd6e298004cbe0
archisuko <99643416+archisuko@users.noreply.github.com>
2022-02-23 11:45:45 +0300

    Add files via upload

---
+++ encapsulation/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+ogrenci bir = new ogrenci("atay",50,"ali");
+bir.adıGetir();
+bir.notuGetir();
+
+
+class ogrenci {
+    string name;
+    string surname;
+    int note;
+
+    public ogrenci(string surname, int note, string name)
+    {
+        Surname = surname;
+        Note = note;
+        Name = name;
+    }
+    public ogrenci(){}
+
+    public string Name { get => name; set => name = value; }
+    public int Note { get => 1; set => note = value; }
+    public string Surname { get => surname; set => surname = value; }
+
+    public void adıGetir() {
+        Console.WriteLine("Adı: "+Name);
+    }
+    public void notuGetir() {
+        Console.WriteLine("Not: "+Note);
+    }
+}
