128560b794c253319a7e55584e1c48c6ca2a2637
archisuko <99643416+archisuko@users.noreply.github.com>
2022-02-23 10:10:26 +0300

    Add files via upload

---
+++ class/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Employee employee1 = new();
+employee1.name = "Sukru";
+employee1.no = 255;
+
+employee1.info();
+
+class Employee {
+    public string name;
+    public string surname;
+    public int no;
+    public string department;
+
+    public void info() {
+        Console.WriteLine("Empleyee Name: {0}",name);
+        Console.WriteLine("Empleyee Name: {0}",no);
+    } 
+}
