19849c5d5c716726cb814a491eedffbff6ea0a25
archisuko <99643416+archisuko@users.noreply.github.com>
2022-02-23 10:27:57 +0300

    Add files via upload

---
+++ class constructor/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Employee employee2 = new("Ali","Atay",789,"Game");
+
+employee2.info();
+
+class Employee {
+    public string name;
+    public string surname;
+    public int no;
+    public string department;
+
+    public void info() {
+        Console.WriteLine("Empleyee Name: {0}",name);
+        Console.WriteLine("Empleyee Name: {0}",no);
+    } 
+
+    public Employee(string Name,string Surname,int No,string Department) {
+        name = Name;
+        surname = Surname;
+        no = No;
+        department = Department;
+    }
+}
